7d2f549baf91ed54110a3a32f09027728df8c247
prasanna <margani.devops@gmail.com>
2016-03-16 18:42:05 -0700

    code and file locations for creating a user by prasanna

--- script.sh
+++ script.sh
@@ -10,7 +10,8 @@ service httpd start
 
 
 Hi Code for creating user in puppet by prasanna
-
+In this below file
+vi /etc/puppet/modules/useradd/manifests/site.pp 
 class useradd {
 user {"prasanna":
     ensure => present
@@ -18,3 +19,12 @@ user {"prasanna":
 
 }
 ~ 
+
+
+Then in the below file write this code 
+
+cd /etc/puppet/manifests/init.pp
+
+node 'client.localdomain' {
+include useradd }
+
